c298ddb2a59b67860eba5134ced52d64a8522d83
Fernando-Braulio <fernandobraulio@outlook.com.br>
2020-06-26 13:37:16 -0300

    Finalizando o menu, fazendo com que ao clicar em uma opção sejá direcionado para a tela

--- lib/common/custom_drawer/widgets/icon_section.dart
+++ lib/common/custom_drawer/widgets/icon_section.dart
@@ -1,42 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:xlo/blocs/drawer_bloc.dart';
 import 'package:xlo/common/custom_drawer/widgets/icon_title.dart';
 
 class IconSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        IconTitle(
-          label: 'Anúncios',
-          iconData: Icons.list,
-          onTap: () {},
-          highlighted: false,
-        ),
-        IconTitle(
-          label: 'Inserir Anúncio',
-          iconData: Icons.edit,
-          onTap: () {},
-          highlighted: false,
-        ),
-        IconTitle(
-          label: 'Chat',
-          iconData: Icons.chat,
-          onTap: () {},
-          highlighted: false,
-        ),
-        IconTitle(
-          label: 'Favoritos',
-          iconData: Icons.favorite,
-          onTap: () {},
-          highlighted: false,
-        ),
-        IconTitle(
-          label: 'Minha Conta',
-          iconData: Icons.person,
-          onTap: () {},
-          highlighted: false,
-        ),
-      ],
-    );
+    final DrawerBloc _drawerBloc = Provider.of<DrawerBloc>(context);
+
+    void _setPage(int page) {
+      Navigator.of(context).pop();
+      _drawerBloc.setPage(page);
+    }
+
+    return StreamBuilder<Object>(
+        stream: _drawerBloc.outPage,
+        builder: (context, snapshot) {
+          return Column(
+            children: <Widget>[
+              IconTitle(
+                label: 'Anúncios',
+                iconData: Icons.list,
+                onTap: () {
+                  _setPage(0);
+                },
+                highlighted: snapshot.data == 0,
+              ),
+              IconTitle(
+                label: 'Inserir Anúncio',
+                iconData: Icons.edit,
+                onTap: () {
+                  _setPage(1);
+                },
+                highlighted: snapshot.data == 1,
+              ),
+              IconTitle(
+                label: 'Chat',
+                iconData: Icons.chat,
+                onTap: () {
+                  _setPage(2);
+                },
+                highlighted: snapshot.data == 2,
+              ),
+              IconTitle(
+                label: 'Favoritos',
+                iconData: Icons.favorite,
+                onTap: () {
+                  _setPage(3);
+                },
+                highlighted: snapshot.data == 3,
+              ),
+              IconTitle(
+                label: 'Minha Conta',
+                iconData: Icons.person,
+                onTap: () {
+                  _setPage(4);
+                },
+                highlighted: snapshot.data == 4,
+              ),
+            ],
+          );
+        });
   }
 }
